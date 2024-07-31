ef440742314e10c7bff6d8985fd42d3b739175c9
dev-salman1 <salmankhalidg504@gmail.com>
2024-08-01 00:58:16 +0500

    Counter Animated

--- App.tsx
+++ App.tsx
@@ -1,3 +1,10 @@
-import App from "./src";
+import MyApp from './src';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+const App = () => (
+  <GestureHandlerRootView style={{ flex: 1 }}>
+    <MyApp />
+  </GestureHandlerRootView>
+);  
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,142 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  runOnJS,
+} from "react-native-reanimated";
+import { AntDesign } from "@expo/vector-icons";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+
+const BUTTON_WIDTH = 250;
+const ICON_SIZE = 25;
+const MAX_SLIDE_OFFSET = BUTTON_WIDTH * 0.4;
+
+const clamp = (value: number, lowerBound: number, upperBound: number) => {
+  "worklet";
+  return Math.min(Math.max(lowerBound, value), upperBound);
+};
 
 const App = () => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const [count, setCount] = useState(0);
+  const scale = useSharedValue(1);
+
+  const incrementCount = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrementCount = () => {
+    setCount((prev) => prev - 1);
+  };
+  const resetCount = () => {
+    setCount(0);
+  };
+  const panGesture = Gesture.Pan()
+    .onTouchesUp(() => {
+      runOnJS(incrementCount)();
+    })
+    .onUpdate((event) => {
+      translateX.value = clamp(
+        event.translationX,
+        -MAX_SLIDE_OFFSET,
+        MAX_SLIDE_OFFSET
+      );
+      translateY.value = clamp(event.translationY, 0, MAX_SLIDE_OFFSET * 0.5);
+    })
+    .onEnd(() => {
+      if (translateY.value === MAX_SLIDE_OFFSET * 0.5) {
+        runOnJS(resetCount)();
+      } else if (translateX.value > MAX_SLIDE_OFFSET * 0.7) {
+        runOnJS(incrementCount)();
+      } else if (translateX.value == -MAX_SLIDE_OFFSET && count > 0) {
+        runOnJS(decrementCount)();
+      }
+      translateX.value = withSpring(0);
+      translateY.value = withSpring(0);
+      scale.value = withSpring(1);
+    })
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        {
+          translateY: translateY.value,
+        },
+        { scale: scale.value },
+      ],
+    };
+  });
+
+  const rPlusMinusIconStyle = useAnimatedStyle(() => {
+    const opacityX = interpolate(
+      translateX.value,
+      [-MAX_SLIDE_OFFSET, 0, MAX_SLIDE_OFFSET],
+      [0.4, 0.9, 0.4]
+    );
+
+    const OpacityY = interpolate(
+      translateY.value,
+      [0, MAX_SLIDE_OFFSET * 0.5],
+      [1, 0]
+    );
+    return {
+      opacity: opacityX * OpacityY,
+    };
+  });
+
+  const rCloseIconStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      translateY.value,
+      [0, MAX_SLIDE_OFFSET * 0.5],
+      [0, 0.8]
+    );
+    return {
+      opacity: opacity,
+    };
+  });
+
+  const rButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value * 0.1 },
+        {
+          translateY: translateY.value * 0.1,
+        },
+      ],
+    };
+  });
+
   return (
     <View style={styles.container}>
-      
+      <Animated.View style={[styles.button, rButtonStyle]}>
+        <Animated.View style={rPlusMinusIconStyle}>
+          <AntDesign name="minus" size={ICON_SIZE} color="#F7F7F7" />
+        </Animated.View>
+        <Animated.View style={rCloseIconStyle}>
+          <AntDesign name="close" size={ICON_SIZE} color="#F7F7F7" />
+        </Animated.View>
+
+        <Animated.View style={rPlusMinusIconStyle}>
+          <AntDesign name="plus" size={ICON_SIZE} color="#F7F7F7" />
+        </Animated.View>
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <GestureDetector gesture={panGesture}>
+            <Animated.View style={[styles.circle, rStyle]}>
+              <Text style={styles.count}>{count}</Text>
+            </Animated.View>
+          </GestureDetector>
+        </Animated.View>
+      </Animated.View>
     </View>
   );
 };
@@ -14,8 +146,36 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#333333",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    width: BUTTON_WIDTH,
+    height: 80,
+    backgroundColor: "#1E1E1E",
+    borderCurve: "continuous",
+    borderRadius: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  circle: {
+    backgroundColor: "#444444",
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 3 },
+    elevation: 0,
+    shadowOpacity: 0.6,
+    shadowRadius: 5,
+    position: "absolute",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  count: {
+    fontSize: 25,
+    color: "#F7F7F7",
   },
 });
